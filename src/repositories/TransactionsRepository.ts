5a93951cac93740b696575c718b45c6c2aee68a8
Gabriel-A-Rocha <gabriel.altair.rocha@gmail.com>
2020-06-25 02:18:01 -0300

    implement transaction creation and balance listing

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,7 +6,19 @@ interface Balance {
   total: number;
 }
 
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+interface Inquiry {
+  transactions: Transaction[];
+  balance: Balance;
+}
+
 class TransactionsRepository {
+  // vetor que registra as transações
   private transactions: Transaction[];
 
   constructor() {
@@ -14,15 +26,37 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    let totalIncome = 0;
+    let totalOutcome = 0;
+
+    this.transactions.forEach(transaction => {
+      if (transaction.type === 'income') {
+        totalIncome += transaction.value;
+      } else {
+        totalOutcome += transaction.value;
+      }
+    });
+
+    const balance: Balance = {
+      income: totalIncome,
+      outcome: totalOutcome,
+      total: totalIncome - totalOutcome,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: TransactionDTO): Transaction {
+    // criar uma instância da classe 'Transaction'
+    const newTransaction = new Transaction({ title, value, type });
+    // registrar transação no array
+    this.transactions.push(newTransaction);
+    // retornar a transação registrada
+    return newTransaction;
   }
 }
 
--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,15 +1,24 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+// criar um repositório de transações
+const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+    const transactionList = transactionsRepository.all();
+    const balance = transactionsRepository.getBalance();
+
+    const inquiry = {
+      transactions: transactionList,
+      balance,
+    };
+
+    return response.json(inquiry);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -17,7 +26,29 @@ transactionRouter.get('/', (request, response) => {
 
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
+    // receber 'title', 'value' e 'type' do corpo da requisição
+    const { title, value, type } = request.body;
+
+    // em caso de retirada, verificar se há saldo
+    if (type === 'outcome') {
+      const balance = transactionsRepository.getBalance();
+      if (balance.total < value) {
+        throw new Error('Saldo insuficiente');
+      }
+    }
+
+    // instanciar o serviço de criação de transação, passando o repositório como parâmetro
+    const createTransactionService = new CreateTransactionService(
+      transactionsRepository,
+    );
+    // acionar o serviço para criar a transação
+    const transaction = createTransactionService.execute({
+      title,
+      value,
+      type,
+    });
+    // retornar a transação registrada
+    return response.json(transaction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
